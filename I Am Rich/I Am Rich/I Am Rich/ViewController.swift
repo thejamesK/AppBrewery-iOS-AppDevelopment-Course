2ce42d4a059bf0e16f49c9a8c2e3538936a44f80
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-06-03 17:30:55 +0200

    Added Label I Am Rich to I Am Rich app

---
+++ I Am Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jakub Krawczyk on 03/06/2019.
+//  Copyright © 2019 Jakub Krawczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
