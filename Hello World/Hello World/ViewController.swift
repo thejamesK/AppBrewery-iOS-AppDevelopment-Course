57e65df1556950678cc4a2541194cc51dabcf4b7
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-06-03 14:14:34 +0200

    First XCode App - Hello World

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jakub Krawczyk on 03/06/2019.
+//  Copyright © 2019 Jakub Krawczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
