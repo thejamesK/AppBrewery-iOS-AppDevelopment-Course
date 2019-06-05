7eb2de7797286b2d45f66941daabe78b117a7d41
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-06-05 23:30:13 +0200

    First step of Dicee App

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Jakub Krawczyk on 05/06/2019.
+//  Copyright © 2019 Jakub Krawczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
