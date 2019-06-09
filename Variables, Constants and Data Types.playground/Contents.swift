9807027d83e3e625de8e361e64b9e6dc1412247a
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-06-09 23:34:19 +0200

    Swift Playgrounds

---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myAge : Int = 32
+
+myAge = 33
+
+let myName : String = "Jakub"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = myName + " Krawczyk"
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+
+let text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let double : Double = 1.453463463
