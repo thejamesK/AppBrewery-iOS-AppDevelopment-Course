529836b0dbf7c64d47549c15607e10570dc917a5
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-06-12 21:51:56 +0200

    99 bottles of beer program

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -1,22 +1,41 @@
 import UIKit
+//
+//let arrayOfNumbers = [1,5,3,6,2,7,23,34]
+//
+//var sum = 0
+//
+//for number in arrayOfNumbers {
+//
+//    sum += number
+//    print(sum)
+//
+//}
+//
+//print("-----------------")
+//
+//
+//
+//for number in 1 ..< 10 where number % 3 == 0{
+//    print(number)
+//}
+//
+//print("-----------------")
 
-let arrayOfNumbers = [1,5,3,6,2,7,23,34]
-
-var sum = 0
-
-for number in arrayOfNumbers {
+func beerSong(forThisManyBottlesOfBeer totalNumberOfBottles : Int) -> String {
     
-    sum += number
-    print(sum)
+    var lyrics: String = ""
     
+    for bottle in (1...totalNumberOfBottles).reversed() {
+        var newLine: String = "\n\(bottle) bottles of beer on the wall, \(bottle) bottles of beer. \nTake one down and pass it around - \(bottle - 1) bottles of beer on the wall.\n"
+        if bottle - 1 == 0 {
+            newLine = "\n\(bottle) bottle of beer on the wall, \(bottle) bottle of beer. \nTake one down and pass it around - no more bottles of beer on the wall.\n"
+        }
+        lyrics += newLine
+    }
+    
+    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, \(totalNumberOfBottles) bottles of beer on the wall.\n"
+    
+    return lyrics
 }
 
-print("-----------------")
-
-
-
-for number in 1 ..< 10 where number % 3 == 0{
-    print(number)
-}
-
-
+print(beerSong(forThisManyBottlesOfBeer : 99))
