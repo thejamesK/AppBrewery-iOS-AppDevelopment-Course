a232555b5e0930f359a38364da810c9790329f7f
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-06-17 19:41:02 +0200

    updated quizz app

--- Quizzler-iOS12/Quizzler/ViewController.swift
+++ Quizzler-iOS12/Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNubmer : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +22,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
-
 
     @IBAction func answerPressed(_ sender: AnyObject) {
   
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+
+        if questionNubmer < 12 {
+            
+            checkAnswer()
+            questionNubmer = questionNubmer + 1
+            
+            questionLabel.text = allQuestions.list[questionNubmer].questionText
+        } else {
+            print("END OF ARRAY")
+        }
+        
+        
     }
     
     
@@ -40,6 +62,15 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
+        
+        let correctAnswer = allQuestions.list[questionNubmer].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        }
+        else {
+            print("Wrong!")
+        }
         
     }
     
