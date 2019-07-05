182335353d583aa2033ab647ab055e79fdd497ef
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-07-05 17:35:37 +0200

    module 12 of udemy course

---
+++ Classes and Object/Classes and Object/Car.swift
@@ -0,0 +1,34 @@
+//
+//  Car.swift
+//  Classes and Object
+//
+//  Created by Jakub Krawczyk on 05/07/2019.
+//  Copyright © 2019 Jakub Krawczyk. All rights reserved.
+//
+
+import Foundation
+
+enum CarType {
+    case Sedan
+    case Coupe
+    case Hatchback
+}
+
+class Car {
+    
+    var colour = "Black"
+    var numberOfSeats = 5
+    var typeOfCar : CarType = .Coupe
+    
+    
+    init() {
+        
+    }
+    
+    convenience init (customerChosenColour : String) {
+        self.init()
+        colour = customerChosenColour
+    }
+    
+    
+}
---
+++ Classes and Object/Classes and Object/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Classes and Object
+//
+//  Created by Jakub Krawczyk on 05/07/2019.
+//  Copyright © 2019 Jakub Krawczyk. All rights reserved.
+//
+
+import Foundation
+
+let myCar = Car()
+
+let someRichGuysCar = Car(customerChosenColour: "Gold")
+
+print(myCar.colour)
+print(myCar.numberOfSeats)
+print(myCar.typeOfCar)
+
+print(someRichGuysCar.colour)
+print(someRichGuysCar.numberOfSeats)
+print(someRichGuysCar.typeOfCar)
+
+
+
