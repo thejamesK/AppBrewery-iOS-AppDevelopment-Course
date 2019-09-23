8b82ff4dcbb62498ec9339fff25568cd42e2455b
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2019-09-23 18:51:35 +0200

    Calculator app
    advanced swift in calculator app

---
+++ Calculator-iOS12/Calculator/Controller/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+
+        if let calcMethod = sender.currentTitle {
+            
+            let calculator = CalculatorLogic(number: displayValue)
+            
+            guard let result = calculator.calculate(symbol: calcMethod) else {
+                fatalError("The results of the calculation is nil.")
+            }
+            displayValue = result
+
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+    
+    }
+
+}
+
---
+++ Calculator-iOS12/Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Jakub Krawczyk on 23/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) -> Double? {
+        
+        if symbol == "+/-" {
+            return number * -1 // displayValue = displayValue * -1
+        } else if symbol == "AC" {
+            return 0
+        } else if symbol == "%" {
+            return number * 0.01
+        }
+        
+        return nil
+    }
+}
